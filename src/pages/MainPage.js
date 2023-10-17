ff4c44c8fada49c7e4d6e46634f8c1033c1ff25f
Anushka7310 <anushkaguptably@gmail.com>
2023-10-17 16:31:10 +0530

    added filter functionality

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,9 +5,10 @@ import PostCard from "../components/PostCard";
 function MainPage() {
   const [blogPosts, setBlogPosts] = useState([]);
   const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(8);
   const [loading, setLoading] = useState(false);
-
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const perPage = 8;
   const scrollToPosts = () => {
     window.scrollTo({
       top: window.innerHeight,
@@ -19,12 +20,26 @@ function MainPage() {
     setPage(page + 1);
   };
 
+  const handleSearch = () => {
+    if (searchQuery) {
+      // Filter posts based on searchQuery
+      const filteredPosts = blogPosts.filter((post) =>
+        post.title.includes(searchQuery)
+      );
+      setSearchResults(filteredPosts);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         setLoading(true);
         const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts?_start=${(page - 1) * perPage}&_limit=${perPage}`
+          `https://jsonplaceholder.typicode.com/posts?_start=${
+            (page - 1) * perPage
+          }&_limit=${perPage}`
         );
         setBlogPosts((prevPosts) => [...prevPosts, ...response.data]);
         setLoading(false);
@@ -35,7 +50,7 @@ function MainPage() {
     };
 
     fetchPosts();
-  }, [page, perPage]);
+  }, [page]);
 
   return (
     <div className="bg-gray-100 min-h-screen">
@@ -56,28 +71,61 @@ function MainPage() {
       </div>
 
       <div className="container mx-auto p-8">
-        {/* Most Popular Posts */}
-        {blogPosts.length > 0 && (
-          <section className="mb-8">
-            <h2 className="text-2xl font-semibold mb-4">Most Popular Posts</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-              {blogPosts.slice(0, 4).map((post) => (
-                <PostCard key={post.id} post={post} />
-              ))}
-            </div>
-          </section>
-        )}
+        {/* Search Input */}
+        <div className="mb-4 flex items-center">
+          <input
+            type="text"
+            placeholder="Search by title"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded-md"
+          />
+          <button
+            onClick={handleSearch}
+            className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-full ml-2"
+          >
+            Search
+          </button>
+        </div>
 
-        {/* Latest Posts */}
-        {blogPosts.length > 4 && (
+        {/* Display Search Results or Posts */}
+        {searchResults.length > 0 ? (
           <section className="mb-8">
-            <h2 className="text-2xl font-semibold mb-4">Latest Posts</h2>
+            <h2 className="text-2xl font-semibold mb-4">Search Results</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-              {blogPosts.slice(4).map((post) => (
+              {searchResults.map((post) => (
                 <PostCard key={post.id} post={post} />
               ))}
             </div>
           </section>
+        ) : (
+          <div>
+            {/* Most Popular Posts */}
+            {blogPosts.length > 0 && (
+              <section className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">
+                  Most Popular Posts
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                  {blogPosts.slice(0, 4).map((post) => (
+                    <PostCard key={post.id} post={post} />
+                  ))}
+                </div>
+              </section>
+            )}
+
+            {/* Latest Posts */}
+            {blogPosts.length > 4 && (
+              <section className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">Latest Posts</h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                  {blogPosts.slice(4).map((post) => (
+                    <PostCard key={post.id} post={post} />
+                  ))}
+                </div>
+              </section>
+            )}
+          </div>
         )}
 
         {/* Load More Button */}
